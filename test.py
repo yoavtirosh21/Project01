b58e1b055516013dca8c76ad10091ae12e26b862
yoavtirosh21 <118483020+yoavtirosh21@users.noreply.github.com>
2022-11-19 18:36:43 -0500

    add test and fix workflow

---
+++ test.py
@@ -0,0 +1,12 @@
+import keyboard
+
+keyboard.write('python Bank01.py')
+keyboard.press_and_release('enter')
+keyboard.write('5000')
+keyboard.press_and_release('enter')
+keyboard.write('0.04')
+keyboard.press_and_release('enter')
+keyboard.write('4')
+keyboard.press_and_release('enter')
+keyboard.write('15')
+keyboard.press_and_release('enter')
